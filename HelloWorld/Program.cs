fdecc0fc7734f06d8b9fb8491901354c3f6989f9
gitTom59 <gitTom59@users.noreply.github.com>
2023-09-26 18:18:23 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Tomasz";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Tomasz");
+
+        }
+    }
+}
